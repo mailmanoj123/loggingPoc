4121bba97d15f29b9956a832109e650f3c1718af
mmahali <mmahali@dxc.com>
2021-12-31 20:13:42 +0530

    logging poc init

---
+++ logging-poc/src/main/java/com/poc/logging/demo/LoggingPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.logging.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingPocApplication.class, args);
+	}
+
+}
